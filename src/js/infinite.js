74aec72378e1c3b1ed9c6c751be619f3d9805f8f
lumpy-the-moose <vmaobo@gmail.com>
2022-11-22 04:57:35 +0200

    infinite scroll

---
+++ src/js/infinite.js
@@ -0,0 +1,59 @@
+import fetchImages from './fetch';
+import renderMarkup from './markup';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  width: '360px',
+  position: 'right-top',
+  fontSize: '18px',
+});
+import throttle from 'lodash.throttle';
+
+const gallery = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export default function infiniteScroll(searchQuery, page, per_page) {
+  let triggered = false;
+
+  window.addEventListener(
+    'scroll',
+    throttle(async () => {
+      const height = document.body.offsetHeight;
+      const screenHeight = window.innerHeight;
+      const scrolled = window.scrollY;
+      const threshold = height - screenHeight / 4;
+      const position = scrolled + screenHeight;
+
+      if (position < threshold || triggered) {
+        return;
+      }
+
+      triggered = true;
+      page += 1;
+
+      const fetchedImages = await fetchImages(searchQuery, page, per_page);
+
+      gallery.insertAdjacentHTML('beforeend', renderMarkup(fetchedImages.hits));
+
+      lightbox.refresh();
+
+      if (per_page * page >= fetchedImages.totalHits) {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+
+        triggered = true;
+        return;
+      }
+
+      triggered = false;
+    }, 300)
+  );
+}
---
+++ src/js/load-more.js
@@ -0,0 +1,49 @@
+import fetchImages from './fetch';
+import renderMarkup from './markup';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  width: '360px',
+  position: 'right-top',
+  fontSize: '18px',
+});
+
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export default async function loadMoreHandler(searchQuery, page, per_page) {
+  loadMore.style.display = 'block';
+
+  loadMore.addEventListener('click', async e => {
+    e.target.style.display = 'none';
+    page += 1;
+
+    const fetchedImages = await fetchImages(searchQuery, page, per_page);
+
+    gallery.insertAdjacentHTML('beforeend', renderMarkup(fetchedImages.hits));
+
+    const { height: cardHeight } =
+      gallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2 - 170,
+      behavior: 'smooth',
+    });
+
+    lightbox.refresh();
+
+    e.target.style.display = 'block';
+
+    if (per_page * page >= fetchedImages.totalHits) {
+      loadMore.style.display = 'none';
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  });
+}
